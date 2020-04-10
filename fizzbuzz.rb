b5d66e534bd3681910ab1deba05d7a385a1dc155
Jason Calvo <jason.calvo7@gmail.com>
2020-04-10 17:16:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,16 +2,16 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    return "Fizz"
+  if int % 3 == 0 && int % 5 == 0 
+    return "FizzBuzz"
   end
   
   if int % 5 == 0 
     return "Buzz"
   end
   
-  if int % 3 == 0 && int % 5 == 0 
-    return "FizzBuzz"
+  if int % 3 == 0 
+    return "Fizz"
   end
   
 end
